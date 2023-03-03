9169d22776ffd9781f290921552453e69c95fbc0
arkadiuszbeer <86738093+arkadiuszbeer@users.noreply.github.com>
2023-03-03 13:28:58 +0100

    Update system-dependencies.R
    Signed-off-by: arkadiuszbeer <86738093+arkadiuszbeer@users.noreply.github.com>

--- system-dependencies.R
+++ system-dependencies.R
@@ -23,38 +23,69 @@ if (upgrade_remotes == "true") {
   require(remotes)
 }
 
-os_info <- read.csv("/etc/os-release", sep = "=", header = FALSE)
-v_os_info <- setNames(os_info$V2, os_info$V1)
+os_release_file <- "/etc/os-release"
+if ( file.exists(os_release_file) { # linux-base OS
+  os_info <- read.csv("/etc/os-release", sep = "=", header = FALSE)
+  v_os_info <- setNames(os_info$V2, os_info$V1)
 
-if (v_os_info[["NAME"]] == "Ubuntu") {
-  ubuntu_version <- as.character(v_os_info[["VERSION_ID"]])
-  cat(paste("Ubuntu version: \"", ubuntu_version, "\"\n", sep = ""))
-  sys_deps_for_pkg <- remotes::system_requirements(
-    os = "ubuntu",
-    os_release = ubuntu_version,
-    path = repo_path
-  )
-  sys_pgks <- gsub("^apt-get install -y ", "", sys_deps_for_pkg)
-  sys_pgks <- c("libgit2-dev", sys_pgks) # For installing staged.dependencies
-  has_pkgs <- vapply(
-    sys_pgks,
-    function(pkg) {
-      system2(
-        "sudo",
-        c("dpkg", "-l", pkg),
-        stdout = NULL,
-        stderr = NULL
-      ) == 0
-    },
-    logical(1)
-  )
-  if (any(!has_pkgs)) {
-    system2("sudo", c("apt-get", "update"))
-    system2("sudo", c("apt-get", "install", "-y", sys_pgks[!has_pkgs]))
+  if (v_os_info[["NAME"]] == "Ubuntu") {
+    ubuntu_version <- as.character(v_os_info[["VERSION_ID"]])
+    cat(paste("Ubuntu version: \"", ubuntu_version, "\"\n", sep = ""))
+    sys_deps_for_pkg <- remotes::system_requirements(
+      os = "ubuntu",
+      os_release = ubuntu_version,
+      path = repo_path
+    )
+    sys_pgks <- gsub("^apt-get install -y ", "", sys_deps_for_pkg)
+    sys_pgks <- c("libgit2-dev", sys_pgks) # For installing staged.dependencies
+    has_pkgs <- vapply(
+      sys_pgks,
+      function(pkg) {
+        system2(
+          "sudo",
+          c("dpkg", "-l", pkg),
+          stdout = NULL,
+          stderr = NULL
+        ) == 0
+      },
+      logical(1)
+    )
+    if (any(!has_pkgs)) {
+      system2("sudo", c("apt-get", "update"))
+      system2("sudo", c("apt-get", "install", "-y", sys_pgks[!has_pkgs]))
+    }
+  } else {
+    cat(paste(
+      "System dependencies not implemented for os:",
+      v_os_info[["NAME"]]
+    ))
   }
-} else {
+} else {  # macos 
+  systemname <- sessionInfo()$running
   cat(paste(
-    "System dependencies not implemented for os:",
-    v_os_info[["NAME"]]
-  ))
+    "Checking system",
+    systemname
+    ))
+  if (grepl("macos", systemname, ignore.case = TRUE) {
+    
+    macos_req_for_pkg <- function(pkgname) {
+      sysreqs_url <- sprintf("https://sysreqs.r-hub.io/pkg/%s",pkgname) 
+      res <- httr::HEAD(sysreqs_url)
+      if(res$status_code == 200) {
+        res <- httr::GET(sysreqs_url)  
+        sysreq <- httr::content(res) 
+        sysreq_chars <- unique(unlist(lapply(sysreq, function(x) {
+          x[[1]]$platforms[["OSX/brew"]]
+        })))
+          return(sysreq_chars )
+      }
+    }
+    
+  }else {
+    cat(paste(
+      "System dependencies not implemented for os:",
+      systemname
+    ))
+  }
 }
+    
